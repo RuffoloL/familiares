d339d8586c0efe1d8f1d702735925b6a97c15745
RuffoloL <luciano.ruffolo@hotmail.com.ar>
2023-11-06 16:14:24 -0300

    Agregado views, template y URL, agregado de datos

---
+++ familia/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.urls import path
+
+from familia.views import listar_familiares
+
+
+urlpatterns = [
+    path('familiares/', listar_familiares),
+]    
